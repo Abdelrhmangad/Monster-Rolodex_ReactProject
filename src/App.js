5758ec6663f00ee1c6c3623fd94e939993eedf65
Abdelrhman Gad <abdelrhman.gad2015@yahoo.com>
2020-04-26 16:31:23 +0200

    Adding cardlist component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+<<<<<<< Updated upstream
+=======
+import {CardList} from './Components/card-list/card-list.component';
+>>>>>>> Stashed changes
 class App extends Component{
   
   constructor(){
@@ -27,11 +31,16 @@ class App extends Component{
   render(){
     return(
         <div className="App">
+<<<<<<< Updated upstream
           {
             this.state.monsters.map(monster => {
               return <h1 key={monster.id}>{monster.name  +  monster.id}</h1>
             })
           }
+=======
+          <CardList name="Gad"/>
+          <h1>{this.state.string}</h1>
+>>>>>>> Stashed changes
         </div>
     )
   }
